5097b0e92627459cb4e7329dfff46a70d410bf83
Akrombek <gaybulloyevakrom964@gmail.com>
2024-08-27 14:54:34 +0500

    4-m

---
+++ 4-m/script.js
@@ -0,0 +1,45 @@
+function chiqar() {
+    let oy = +prompt("Oyni raqamini kiriting")
+
+    switch(oy) {
+        case 1:
+            console.log("31 kun bor")
+            break
+        case 2:
+            console.log("28 kun bor")
+            break
+        case 3:
+            console.log("31 kun bor")
+            break
+        case 4:
+            console.log("30 kun bor")
+            break
+        case 5:
+            console.log("31 kun bor")
+            break
+        case 6:
+            console.log("30 kun bor")
+            break
+        case 7:
+            console.log("31 kun bor")
+            break
+        case 8:
+            console.log("31 kun bor")
+            break
+        case 9:
+            console.log("30 kun bor")
+            break
+        case 10:
+            console.log("31 kun bor")
+            break
+        case 11:
+            console.log("30 kun bor")
+            break
+        case 12:
+            console.log("31 kun bor")
+            break
+    }
+}
+
+
+chiqar()
