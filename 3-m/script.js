a915a8882fea731e7d8d5b68587627a9938bcb2f
Akrombek <gaybulloyevakrom964@gmail.com>
2024-08-27 14:39:26 +0500

    3-m

---
+++ 3-m/script.js
@@ -0,0 +1,45 @@
+function chiqar() {
+    let oy = prompt("Oyning sonini kiriting")
+    
+    switch (oy) {
+        case 1:
+            console.log("Qish")
+            break
+        case 2:
+            console.log("Qish")
+            break
+        case 3:
+            console.log("Bahor")
+            break
+        case 4:
+            console.log("Bahor")
+            break
+        case 5:
+            console.log("Bahor")
+            break
+        case 6:
+            console.log("Yoz")
+            break
+        case 7:
+            console.log("Yoz")
+            break
+        case 8:
+            console.log("Yoz")
+            break
+        case 9:
+            console.log("Kuz")
+            break
+        case 10:
+            console.log("Kuz")
+            break
+        case 11:
+            console.log("Kuz")
+            break
+        case 12:
+            console.log("Qish")
+            break
+    }
+}
+
+
+chiqar()
